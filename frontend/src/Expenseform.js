e53800153a8597f4189579ecae9478aaf73c887e
Mahee Uddin <uddinmahee@hotmail.com>
2024-02-20 22:08:08 -0500

    alberta-100%-Nelson

--- frontend/src/Expenseform.js
+++ frontend/src/Expenseform.js
@@ -1,100 +1,82 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 
 const Expenseform = () => {
- /*
+  const [expenseData, setExpenseData] = useState({
+    expenses: [],
+    selection: "Internet",
+    name: "",
+    price: "",
+  });
 
-! Form serves as a basic functionality for now, not complete !
+  const selections = ["Bills", "Food", "Groceries", "Entertainment", "Other"];
 
-TODO: selection tag needs to be an array of selection such as [Internet, Food, Groceries, Uber, Other] etc.
+  const handleChange = (e) => {
+    setExpenseData({ ...expenseData, [e.target.name]: e.target.value });
+  };
 
-TODO: The infoexpense data needs to be configured and displayed as such:
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newExpense = {
+      name: expenseData.name,
+      price: parseFloat(expenseData.price),
+    };
 
-*Name of array as the selection
-*Then has the data of objects inside
- Bills: [
-  {
-   name: "Internet",
-   price: 25,
-  },
+    setExpenseData({
+      ...expenseData,
+      expenses: [
+        ...expenseData.expenses,
+        { [expenseData.selection]: newExpense },
+      ],
+      name: "",
+      price: "",
+    });
 
-  {
-   name: "Phone Bill",
-   price: 25,
-  },
- ]
- */
+    console.log(expenseData);
+  };
 
- //storing data from form, the keys are their values
- const [infoExpense, setInfoExpense] = useState({
-  selection: "",
-  name: "",
-  email: "",
-  phonenumber: "",
- });
-
- // records the changes aka events in the input fields and selection field and stores them in setInfoExpense
- const handleChange = (e) => {
-  setInfoExpense({ ...infoExpense, [e.target.name]: e.target.value });
- };
-
- //submits the form and console logs the data
- const handleSubmit = (e) => {
-  e.preventDefault();
-  console.log(infoExpense);
- };
-
- return (
-  <div>
-   <form onSubmit={handleSubmit}>
+  return (
     <div>
-     <h3>Expense Form</h3>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <h3>Expense Form</h3>
+        </div>
+        <div>
+          <select
+            name="selection"
+            value={expenseData.selection}
+            onChange={handleChange}
+          >
+            {selections.map((category) => (
+              <option key={category} value={category}>
+                {category}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <input
+            type="text"
+            name="name"
+            placeholder="Expense Name"
+            value={expenseData.name}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <input
+            type="number"
+            name="price"
+            placeholder="Expense Price $"
+            value={expenseData.price}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <button type="submit">Submit Expense</button>
+        </div>
+      </form>
     </div>
-    <div>
-     <select
-      name="selection"
-      value={infoExpense.selection}
-      onChange={handleChange}
-     >
-      <option value="C++">c++</option>
-      <option value="Placement">Placement</option>
-      <option value="Java">Java</option>
-      <option value="Python">Python</option>
-     </select>
-    </div>
-    <div>
-     <input
-      type="text"
-      name="name"
-      placeholder="Name"
-      value={infoExpense.name}
-      onChange={handleChange}
-     />
-    </div>
-    <div>
-     <input
-      type="email"
-      name="email"
-      placeholder="Email"
-      value={infoExpense.email}
-      onChange={handleChange}
-     />
-    </div>
-    <div>
-     <input
-      type="number"
-      name="phonenumber"
-      placeholder="Phone Number"
-      value={infoExpense.phonenumber}
-      onChange={handleChange}
-     />
-    </div>
-    <div>
-     <button>Submit Contact</button>
-    </div>
-   </form>
-  </div>
- );
+  );
 };
 
 export default Expenseform;
