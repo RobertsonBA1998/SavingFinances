13392e5785badc497e8bf1328721763425a86767
Mahee Uddin <uddinmahee@hotmail.com>
2024-02-22 13:20:42 -0500

    expenseform-100%

--- frontend/src/Expenseform.js
+++ frontend/src/Expenseform.js
@@ -3,7 +3,15 @@ import { useState } from "react";
 import ExpenseFormData from "./ExpenseFormData";
 
 const Expenseform = () => {
- /*
+  const selectionArray = [
+    "Internet",
+    "Bills",
+    "Food",
+    "Groceries",
+    "Entertainment",
+    "Other",
+  ];
+  /*
 
 ! Form serves as a basic functionality for now, not complete !
 
@@ -26,91 +34,90 @@ TODO: The infoexpense data needs to be configured and displayed as such:
  ]
  */
 
- //storing data from form, the keys are their values
- const [infoExpense, setInfoExpense] = useState({
-  selection: "",
-  name: "",
-  email: "",
-  phonenumber: "",
- });
-
- //storing submitted data from forn
- const [data, setData] = useState([]);
-
- // records the changes aka events in the input fields and selection field and stores them in setInfoExpense
- const handleChange = (e) => {
-  setInfoExpense({ ...infoExpense, [e.target.name]: e.target.value });
- };
-
- //submits the form and console logs the data
- const handleSubmit = (e) => {
-  e.preventDefault();
-  console.log(infoExpense);
-
-  //spread data creates a new array and adds new data to the info expense
-  setData([...data, infoExpense]);
-
-  // text boxes are empty so user can enter new data
-  setInfoExpense({
-   selection: "",
-   name: "",
-   email: "",
-   phonenumber: "",
+  //storing data from form, the keys are their values
+  const [infoExpense, setInfoExpense] = useState({
+    selection: "",
+    name: "",
+    email: "",
+    phonenumber: "",
   });
- };
 
- return (
-  <div>
-   <form onSubmit={handleSubmit}>
+  //storing submitted data from forn
+  const [data, setData] = useState([]);
+
+  // records the changes aka events in the input fields and selection field and stores them in setInfoExpense
+  const handleChange = (e) => {
+    setInfoExpense({ ...infoExpense, [e.target.name]: e.target.value });
+  };
+
+  //submits the form and console logs the data
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(infoExpense);
+
+    //spread data creates a new array and adds new data to the info expense
+    setData([...data, infoExpense]);
+
+    // text boxes are empty so user can enter new data
+    setInfoExpense({
+      selection: "",
+      name: "",
+      email: "",
+      phonenumber: "",
+    });
+  };
+
+  return (
     <div>
-     <h3>Expense Form</h3>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <h3>Expense Form</h3>
+        </div>
+        <div>
+          <select
+            name="selection"
+            value={infoExpense.selection}
+            onChange={handleChange}
+          >
+            {selectionArray.map((category, index) => (
+              <option key={index}>{category}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            value={infoExpense.name}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={infoExpense.email}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <input
+            type="number"
+            name="phonenumber"
+            placeholder="Phone Number"
+            value={infoExpense.phonenumber}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <button>Submit Contact</button>
+        </div>
+      </form>
+      <ExpenseFormData data={data} setData={setData} />
     </div>
-    <div>
-     <select
-      name="selection"
-      value={infoExpense.selection}
-      onChange={handleChange}
-     >
-      <option value="C++">c++</option>
-      <option value="Placement">Placement</option>
-      <option value="Java">Java</option>
-      <option value="Python">Python</option>
-     </select>
-    </div>
-    <div>
-     <input
-      type="text"
-      name="name"
-      placeholder="Name"
-      value={infoExpense.name}
-      onChange={handleChange}
-     />
-    </div>
-    <div>
-     <input
-      type="email"
-      name="email"
-      placeholder="Email"
-      value={infoExpense.email}
-      onChange={handleChange}
-     />
-    </div>
-    <div>
-     <input
-      type="number"
-      name="phonenumber"
-      placeholder="Phone Number"
-      value={infoExpense.phonenumber}
-      onChange={handleChange}
-     />
-    </div>
-    <div>
-     <button>Submit Contact</button>
-    </div>
-   </form>
-   <ExpenseFormData data={data} setData={setData} />
-  </div>
- );
+  );
 };
 
 export default Expenseform;
